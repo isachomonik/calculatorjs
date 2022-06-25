a38d6199ded6d740ba6aed61526311c37fb63811
Isac Homonik <dev.isachomonik@gmail.com>
2022-06-25 17:35:29 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var num1 = 30;
+var num2 = 10;
+var operator = "x";
+
+switch (operator) {
+  case "*":
+    console.log(num1 * num2);
+    break;
+  case "/":
+    console.log(num1 / num2);
+    break;
+  case "+":
+    console.log(num1 + num2);
+    break;
+  case "-":
+    console.log(num1 - num2);
+    break;
+  default:
+    console.log("Operação não concluida");
+}
